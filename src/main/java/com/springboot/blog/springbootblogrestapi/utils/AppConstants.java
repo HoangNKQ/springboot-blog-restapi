48df9b1ef1dd90d42190d6db7eb9948b202bafa9
HoagOWL <nkqhoangtu@gmail.com>
2022-08-31 12:41:40 +0700

    Refactoring Default Parameters

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/utils/AppConstants.java
@@ -0,0 +1,11 @@
+package com.springboot.blog.springbootblogrestapi.utils;
+
+public class AppConstants {
+
+    // Create constant for default request parameters
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "5";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+
+}
