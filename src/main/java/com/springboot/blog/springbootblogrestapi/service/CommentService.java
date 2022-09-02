175ff7bf3f2f1c7b9e84609e5892ff135e91cc4f
HoagOWL <nkqhoangtu@gmail.com>
2022-09-03 02:18:24 +0700

    Add Create Comment Rest Api

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.springboot.blog.springbootblogrestapi.service;
+
+import com.springboot.blog.springbootblogrestapi.payload.CommentDto;
+
+public interface CommentService {
+
+    CommentDto createComment(long postId, CommentDto commentDto);
+}
