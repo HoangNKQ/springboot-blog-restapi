ee3fd4262a5fc2c01fc6d7459dcb0993984fed7c
HoagOWL <nkqhoangtu@gmail.com>
2022-09-13 01:40:01 +0700

    Add Model Mapper. Use Model Mapper to convert between Entity and Dto

--- src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestapiApplication.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestapiApplication.java
@@ -1,10 +1,18 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBlogRestapiApplication {
+
+	// Add Model mapper to map Entity and DTO
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestapiApplication.class, args);
