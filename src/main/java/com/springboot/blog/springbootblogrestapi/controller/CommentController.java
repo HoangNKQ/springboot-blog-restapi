fe9c4baccce6b3c68678e146d95eadbf5f29016a
HoagOWL <nkqhoangtu@gmail.com>
2022-09-19 01:11:22 +0700

    Validate Create Comment and Update Comment API with Java Validation

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/CommentController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.springbootblogrestapi.controller;
 
 import com.springboot.blog.springbootblogrestapi.payload.CommentDto;
 import com.springboot.blog.springbootblogrestapi.service.CommentService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +21,8 @@ public class CommentController {
     }
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
-                                                    @RequestBody CommentDto commentDto) {
+    public ResponseEntity<CommentDto> createComment( @PathVariable(value = "postId") long postId,
+                                                     @Valid @RequestBody CommentDto commentDto) {
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
@@ -39,7 +40,7 @@ public class CommentController {
     @PutMapping("posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") long postID,
                                                     @PathVariable(value = "commentId") long commentId,
-                                                    @RequestBody CommentDto commentRequest) {
+                                                    @Valid @RequestBody CommentDto commentRequest) {
         return new ResponseEntity<>(commentService.updateComment(postID, commentId, commentRequest), HttpStatus.OK);
     }
 
